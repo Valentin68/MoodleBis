538a4f824cd3cad3c538a0ab4f30385f98b0eaba
Valentin68 <50031978+Valentin68@users.noreply.github.com>
2020-01-24 23:47:40 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	$sitename = "MoodleBis";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $sitename ?></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>	
+<body>
+	<h1>Bienvenue sur <?php echo $sitename ?> !</h1>
+</body>
+</html>
